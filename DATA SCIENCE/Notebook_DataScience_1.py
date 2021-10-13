5a08e74302ca7cc5c267f86bfdd95c55217e600b
Joey Tran <duytran.arch@gmail.com>
2021-10-13 14:42:07 +0700

    Update NB

--- DATA SCIENCE/Notebook_DataScience_1.py
+++ DATA SCIENCE/Notebook_DataScience_1.py
@@ -13,8 +13,6 @@ from pandas.core.series import Series
 path_excel_memo = r"K:\_WFH VIETMY\XLSX\MEMORY.xlsx"
 memo_df2 = pd.read_excel(path_excel_memo,sheet_name = "Sheet1",index_col=0).dropna()
 
-dic = memo_df2.to_dict()["Memory Value"]
-for d in dic:
-    dic[d] = list(dic[d])
+dic = memo_df2.to_dict()
 print(dic)
 
